349143a54b5fd2906e701ee14746a86449da1e93
Jotaro Sugiyama <sugi.jotaro@gmail.com>
2025-05-16 17:03:55 +0900

    Update splitAndAnalyzeVideoMain.ts

--- splitAndAnalyzeVideoMain.ts
+++ splitAndAnalyzeVideoMain.ts
@@ -257,7 +257,20 @@ async function splitVideo(inputPath: string, outDir: string): Promise<string[]> 
     const end = Math.min(start + segmentLength, totalSeconds);
     const outName = `part${idx}_${baseName}_${start}-${Math.floor(end)}.mp4`;
     const outPath = path.join(outDir, outName);
-    const drawtext = "drawtext=fontfile=/System/Library/Fonts/Helvetica.ttc:text='%{pts\\:hms}':x=(w-text_w)/2:y=h-th-10:fontcolor=white:fontsize=48:box=1:boxcolor=black@0.5:boxborderw=10";
+    const minExpr = "%{eif\\:floor(t/60)\\:d\\:2}";
+    const secExpr = "%{eif\\:mod(t\\,60)\\:d\\:2}";
+    const drawtext = [
+      "drawtext=fontfile=/System/Library/Fonts/Helvetica.ttc",
+      // `text='${minExpr}:${secExpr}'`,
+      `text='%{pts\\:hms}`,
+      "x=(w-text_w)/2",
+      "y=h-th-10",
+      "fontcolor=white",
+      "fontsize=48",
+      "box=1",
+      "boxcolor=black@0.5",
+      "boxborderw=10"
+    ].join(':');
     const vf = `scale=1280:720,fps=1,${drawtext}`;
     const ffmpegArgs = [
       '-loglevel', 'error',
